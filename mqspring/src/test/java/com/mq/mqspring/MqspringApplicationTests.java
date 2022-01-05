04fbd97099859b5e2b06bcd10ce9bfd9a30d1c22
jcpatil25 <jcpatil25@gmail.com>
2022-01-05 19:18:39 +0530

    fist commit

---
+++ mqspring/src/test/java/com/mq/mqspring/MqspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mq.mqspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
